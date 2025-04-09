c4522acbc19eb17923a0a098f4a7f06f0eaabf15
RANOELISON Dimbisoa Patrick <patdimby@outlook.fr>
2025-04-09 17:44:07 +0300

    add components
    bruts components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,19 @@
 import { Routes } from '@angular/router';
-import { SearchFormComponent } from './components/search-form/search-form.component';
+import { SearchComponent } from './pages/website/search/search.component';
+import { BookFlightComponent } from './pages/website/book-flight/book-flight.component';
+import { MyBookingsComponent } from './pages/website/my-bookings/my-bookings.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
 
 export const routes: Routes = [
-  { path: 'search', component: SearchFormComponent },
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent, title:'Search Flight' },
+  { path: 'book-flight', component: BookFlightComponent, title:'Book new ticket' },
+  { path: 'bookings', component: MyBookingsComponent, title:'My-Bookings' },
+  { path: '', component:LayoutComponent,
+    children:[
+      {
+        
+      }
+    ]
+   },
 ];
